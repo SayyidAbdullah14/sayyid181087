70813de9ff353dc694a92d59532e9ce90ee6e2f9
acer <acer@someplace.com>
2021-12-17 20:50:12 +0700

    project

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,11 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('game/', views.game, name='game'),
+    path('D2B/', views.D2B, name='D2B'),
+    path('D2H/', views.D2H, name='D2H'),
+    path('B2D/', views.B2D, name='B2D'),
+    path('B2H/', views.B2H, name='B2H'),
+    path('H2D/', views.H2D, name='H2D'),
+    path('H2B/', views.H2B, name='H2B'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+def game(request):
+    return render(request, 'main/game.html')
+
+def D2B(request):
+    return render(request, 'main/D2B.html')
+
+def D2H(request):
+    return render(request, 'main/D2H.html')
+
+def B2D(request):
+    return render(request, 'main/B2D.html')
+
+def B2H(request):
+    return render(request, 'main/B2H.html')
+
+def H2D(request):
+    return render(request, 'main/H2D.html')
+
+def H2B(request):
+    return render(request, 'main/H2B.html')
