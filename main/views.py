e85b25b779ae1d19c4075bacba543d966325ee9a
acer <acer@someplace.com>
2021-12-20 10:11:13 +0700

    UAS

--- main/views.py
+++ main/views.py
@@ -4,23 +4,86 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'main/home.html')
 
+
 def game(request):
     return render(request, 'main/game.html')
 
+
 def D2B(request):
-    return render(request, 'main/D2B.html')
+    if request.method == 'POST':
+        bilmasuk = int(request.POST['masuk'])
+
+        bilkeluar = str('123')
+        context = {'masuk': bilmasuk,
+                   'keluar': bilkeluar,
+                   }
+        return render(request, 'main/D2B.html', context)
+
+    else:
+        return render(request, 'main/D2B.html', {})
+
 
 def D2H(request):
-    return render(request, 'main/D2H.html')
+    if request.method == 'POST':
+        bilmasuk = int(request.POST['masuk'])
+
+        bilkeluar = str('123')
+        context = {'masuk': bilmasuk,
+                   'keluar': bilkeluar,
+                   }
+        return render(request, 'main/D2B.html', context)
+
+    else:
+        return render(request, 'main/D2B.html', {})
 
 def B2D(request):
-    return render(request, 'main/B2D.html')
+    if request.method == 'POST':
+        bilmasuk = int(request.POST['masuk'])
+
+        bilkeluar = str('123')
+        context = {'masuk': bilmasuk,
+                   'keluar': bilkeluar,
+                   }
+        return render(request, 'main/D2B.html', context)
+
+    else:
+        return render(request, 'main/D2B.html', {})
 
 def B2H(request):
-    return render(request, 'main/B2H.html')
+    if request.method == 'POST':
+        bilmasuk = int(request.POST['masuk'])
+
+        bilkeluar = str('123')
+        context = {'masuk': bilmasuk,
+                   'keluar': bilkeluar,
+                   }
+        return render(request, 'main/D2B.html', context)
+
+    else:
+        return render(request, 'main/D2B.html', {})
 
 def H2D(request):
-    return render(request, 'main/H2D.html')
+    if request.method == 'POST':
+        bilmasuk = int(request.POST['masuk'])
+
+        bilkeluar = str('123')
+        context = {'masuk': bilmasuk,
+                   'keluar': bilkeluar,
+                   }
+        return render(request, 'main/D2B.html', context)
+
+    else:
+        return render(request, 'main/D2B.html', {})
 
 def H2B(request):
-    return render(request, 'main/H2B.html')
+    if request.method == 'POST':
+        bilmasuk = int(request.POST['masuk'])
+
+        bilkeluar = str('123')
+        context = {'masuk': bilmasuk,
+                   'keluar': bilkeluar,
+                   }
+        return render(request, 'main/D2B.html', context)
+
+    else:
+        return render(request, 'main/D2B.html', {})
