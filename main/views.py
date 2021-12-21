cc4cccafb38ab5514ce89ecec9d94b8722090f1c
acer <acer@someplace.com>
2021-12-21 22:07:19 +0700

    UAS

--- main/views.py
+++ main/views.py
@@ -12,78 +12,96 @@ def game(request):
 def D2B(request):
     if request.method == 'POST':
         bilmasuk = int(request.POST['masuk'])
-
-        bilkeluar = str('123')
+        bilkeluar = bin(bilmasuk).replace("0b", "")
         context = {'masuk': bilmasuk,
                    'keluar': bilkeluar,
                    }
+        print(context)
         return render(request, 'main/D2B.html', context)
-
     else:
         return render(request, 'main/D2B.html', {})
-
 
 def D2H(request):
     if request.method == 'POST':
         bilmasuk = int(request.POST['masuk'])
-
-        bilkeluar = str('123')
+        bilkeluar= int(oct(bilmasuk) .replace("0o",""))
         context = {'masuk': bilmasuk,
                    'keluar': bilkeluar,
                    }
-        return render(request, 'main/D2B.html', context)
-
+        print(context)
+        return render(request, 'main/D2H.html', context)
     else:
-        return render(request, 'main/D2B.html', {})
+        return render(request, 'main/D2H.html', {})
 
 def B2D(request):
     if request.method == 'POST':
         bilmasuk = int(request.POST['masuk'])
-
-        bilkeluar = str('123')
+        a = 10
+        b = 1
+        desimal = 0
+        p = 0
+        while b <= bilmasuk:
+            c = bilmasuk % a
+            d = c / b
+            desimal = desimal + d*(2**p)
+            a = a * 10
+            b = b * 10
+            p = p + 1
+        bilkeluar = int(desimal)
         context = {'masuk': bilmasuk,
                    'keluar': bilkeluar,
                    }
-        return render(request, 'main/D2B.html', context)
+        print(context)
+        return render(request, 'main/B2D.html', context)
 
     else:
-        return render(request, 'main/D2B.html', {})
+        return render(request, 'main/B2D.html', {})
 
 def B2H(request):
     if request.method == 'POST':
         bilmasuk = int(request.POST['masuk'])
-
-        bilkeluar = str('123')
+        bilkeluar = oct(bilmasuk).replace("0o", "")
         context = {'masuk': bilmasuk,
                    'keluar': bilkeluar,
                    }
-        return render(request, 'main/D2B.html', context)
-
+        print(context)
+        return render(request, 'main/B2H.html', context)
     else:
-        return render(request, 'main/D2B.html', {})
+        return render(request, 'main/B2H.html', {})
 
 def H2D(request):
     if request.method == 'POST':
         bilmasuk = int(request.POST['masuk'])
-
-        bilkeluar = str('123')
+        bilkeluar = bin(bilmasuk).replace("0b", "")
+        bilmasuk = int(bilkeluar)
+        a = 10
+        b = 1
+        desimal = 0
+        p = 0
+        while b <= bilmasuk:
+            c = bilmasuk % a
+            d = c / b
+            desimal = desimal + d * (2 ** p)
+            a = a * 10
+            b = b * 10
+            p = p + 1
+        bilkeluar = int(desimal)
         context = {'masuk': bilmasuk,
                    'keluar': bilkeluar,
                    }
-        return render(request, 'main/D2B.html', context)
-
+        print(context)
+        return render(request, 'main/H2D.html', context)
     else:
-        return render(request, 'main/D2B.html', {})
+        return render(request, 'main/H2D.html', {})
 
 def H2B(request):
     if request.method == 'POST':
         bilmasuk = int(request.POST['masuk'])
-
-        bilkeluar = str('123')
+        bilkeluar = bin(bilmasuk).replace("0b", "")
         context = {'masuk': bilmasuk,
                    'keluar': bilkeluar,
                    }
-        return render(request, 'main/D2B.html', context)
-
+        print(context)
+        return render(request, 'main/H2B.html', context)
     else:
-        return render(request, 'main/D2B.html', {})
+        return render(request, 'main/H2B.html', {})
